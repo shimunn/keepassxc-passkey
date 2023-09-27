00f5777573f297c8efe52c8f0934dfde7eb979d3
shimun <shimun@shimun.net>
2023-09-27 15:35:06 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic flake";
+
+  inputs.keepassxc-passkey = {
+    flake = false;
+    url = "github:varjolintu/keepassxc?ref=feature/support_webauthn";
+  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, keepassxc-passkey }: (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in
+    rec {
+
+      packages.keepassxc-passkey = pkgs.keepassxc-passkey;
+
+      defaultPackage = packages.keepassxc-passkey;
+
+    })) // {
+    overlay = final: prev: {
+      keepassxc-passkey = prev.keepassxc.overrideAttrs (oa: {
+        src = keepassxc-passkey;
+        cmakeFlags = oa.cmakeFlags ++ [ "-DWITH_XC_BROWSER_PASSKEYS=ON" ];
+      });
+      nixosModule = ({ config, pkgs, lib, ... }: { });
+    };
+  };
+}
